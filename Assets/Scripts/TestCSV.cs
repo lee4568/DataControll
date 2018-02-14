bd4e17cdce4f3b95e799c1fe726ae9900dffee87
lee4568 <dlfdlfgh@naver.com>
2018-02-14 17:45:06 +0900

    Initial commit

---
+++ Assets/Scripts/TestCSV.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System.Text;
+
+public class TestCSV : MonoBehaviour {
+
+    public string csvFileName;
+    
+
+    public GameObject unitPrefab;
+
+	void Start ()
+    {
+       
+        List<Dictionary<string, object>> data = CSVReader.Read(csvFileName);
+        //for (int i = 0; i < data.Count; i++)
+        //{
+        //   Debug.Log(i + "번의 ::: " + "종족은 ::: " + data[i]["종족"] + " ::: 이름은 ::: "+ data[i]["유닛이름"] + " ::: " +" HP::: "+ data[i]["체력"]);
+        //}
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            GameObject unitOBJ = Instantiate(unitPrefab) as GameObject;
+            UnitSc unitDetail = unitOBJ.GetComponent<UnitSc>();
+            switch (data[i]["종족"].ToString())
+            {
+                case "프로토스":
+                    unitDetail.unitTribe = UnitSc.UNITTRIBE.PROTOSS;
+                    break;
+                case "저그":
+                    unitDetail.unitTribe = UnitSc.UNITTRIBE.ZERG;
+                    break;
+                case "테란":
+                    unitDetail.unitTribe = UnitSc.UNITTRIBE.TERRAN;
+                    break;
+            }
+            unitDetail.unitName = data[i]["유닛이름"].ToString();
+            unitDetail.Hp = int.Parse(data[i]["체력"].ToString());
+            unitDetail.Mp = int.Parse(data[i]["마나"].ToString());
+            unitDetail.Atk = int.Parse(data[i]["공격력"].ToString());
+            unitDetail.Def = int.Parse(data[i]["방어력"].ToString());
+            unitDetail.Shd = int.Parse(data[i]["쉴드"].ToString());
+            unitDetail.unitAsp = float.Parse(data[i]["공격속도"].ToString());
+            unitDetail.unitMsp = float.Parse(data[i]["이동속도"].ToString());
+           unitDetail.unitRange = float.Parse(data[i]["사거리"].ToString());
+         }
+    }
+}
---
+++ Assets/Scripts/UnitSc.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSc : MonoBehaviour
+{
+
+    public enum UNITTRIBE
+    {
+        NEUTRAL = 0,
+        PROTOSS,
+        ZERG,
+        TERRAN
+    }
+    public UNITTRIBE unitTribe;
+    public string unitName;
+    public int Hp;
+    public int Mp;
+    public int Atk;
+    public int Def;
+    public int Shd;
+    public float unitAsp;
+    public float unitMsp;
+    public float unitRange;
+
+    void Start()
+    {
+        gameObject.name = unitName;
+    }
+
+}
