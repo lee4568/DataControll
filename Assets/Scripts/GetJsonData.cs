c5456ec90ff23ede587a6325b35985dbe6d82495
lee4568 <dlfdlfgh@naver.com>
2018-02-23 17:14:58 +0900

    unit

---
+++ Assets/Scripts/GetJsonData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+
+public class GetJsonData : MonoBehaviour {
+
+    public TextAsset jsonData;
+    public Dictionary<string, object> UnitData;
+    public GameObject Unit;
+
+	void Start ()
+   {
+        //    string unitData = Json.Serialize(jsonData);
+        //    Debug.Log(unitData);
+        var dict = Json.Deserialize(jsonData.ToString()) as Dictionary<string, object>; 
+        List<object> unitInfo = dict["UnitInfo"] as List<object>;
+        for (int i = 0; i < unitInfo.Count; i++)
+        {
+            Dictionary<string, object> unitData = unitInfo[i] as Dictionary<string, object>;
+            GameObject UnitOBJ = Instantiate(Unit) as GameObject;
+
+            UnitOBJ.GetComponent<UnitSc>().unitName = unitData["이름"].ToString();
+            UnitOBJ.GetComponent<UnitSc>().Hp = int.Parse(unitData["체력"].ToString());
+            UnitOBJ.GetComponent<UnitSc>().Mp = int.Parse(unitData["마나"].ToString());
+            UnitOBJ.GetComponent<UnitSc>().Atk = int.Parse(unitData["공격력"].ToString());
+        }
+
+    }
+	 
+}
---
+++ Assets/Scripts/GetUserData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+using UnityEngine.UI;
+
+public class GetUserData : MonoBehaviour {
+
+    public TextAsset userData;
+    public Text[] text;
+    int Win = 0;
+    int Lose = 0;
+
+    void Start ()
+    {
+        var dict = Json.Deserialize(userData.ToString()) as Dictionary<string, object>; 
+        Win = int.Parse(dict["win"].ToString());
+        Lose = int.Parse(dict["lose"].ToString());
+        float percent = (float)Win / ((float)Win + (float)Lose)*100;
+
+        text[0].text = ("ID : " + dict["userID"]).ToString();
+        text[1].text = ("닉네임 : " + dict["username"]).ToString();
+        text[2].text = ("승리 : " + dict["win"]).ToString();
+        text[3].text = ("패배 : " + dict["lose"]).ToString();
+        text[4].text = (dict["lastedconnect"]).ToString();
+        text[5].text = ("승률 : " + (float)percent).ToString();
+
+    }
+
+
+    void Update ()
+    {
+
+    }
+}
